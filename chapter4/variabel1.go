7bfa1a6da2e4f94c32140e572ae07f8fe37e16e8
Frey <freyandhy@gmail.com>
2017-01-06 02:18:36 +0800

    Added Chapter 4 - Variables

---
+++ chapter4/variabel1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+    // First Method
+    var x string = "Variable X"
+    fmt.Println(x)
+
+    // Second Method
+    var y string
+    y = "Variable Y"
+    fmt.Println(y)
+
+    // Third Method
+    z := "Variable Z"
+    fmt.Println(z)
+
+    n := 10
+    fmt.Println(n)
+}
